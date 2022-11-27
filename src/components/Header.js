c1d5f2541adccbb416c83b07e7200c7a514e4661
jrero99 <loljavi.redondo@gmail.com>
2022-11-27 10:03:58 +0100

    Remove class

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ const Header = () => {
       <nav className="h-20 py-2 px-6 max-w-screen-xl flex justify-end m-auto">
         <ul className="flex">
           <li className="flex ">
-            <a className="flex text-gray-900 p-4 font-semibold text-base cursor-pointer hover:text-blue" href="https://github.com/jrero99" target="_blank">
+            <a className="flex text-gray-900 p-4 font-semibold text-base cursor-pointer" href="https://github.com/jrero99" target="_blank">
               github.
             </a>
           </li>
           <li className="flex">
-            <a className="flex text-gray-900 p-4 font-semibold text-base cursor-pointer hover:text-blue" href="https://www.linkedin.com/in/redondorodriguezjavier/" target="_blank">
+            <a className="flex text-gray-900 p-4 font-semibold text-base cursor-pointer" href="https://www.linkedin.com/in/redondorodriguezjavier/" target="_blank">
               linkedin.
             </a>
           </li>
